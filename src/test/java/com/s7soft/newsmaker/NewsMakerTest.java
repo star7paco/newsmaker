9a97baf10974f47bcbbfc6d8a2c3c2949647b1a9
mhoshi <mhoshi@so-netmedia.jp>
2016-01-08 18:29:16 +0900

    assertJテストサンプル追加

--- src/test/java/com/s7soft/newsmaker/NewsMakerTest.java
+++ src/test/java/com/s7soft/newsmaker/NewsMakerTest.java
@@ -1,7 +1,19 @@
 package com.s7soft.newsmaker;
 
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
 
-public class NewsMakerTest{
+import org.junit.Test;
 
+public class NewsMakerTest {
+
+
+	 @Test
+	public void AssertJの基本的な使い方() {
+
+		String actual = "愛媛 蜜柑";
+		assertThat(actual, startsWith("愛媛"));
+
+	}
 
 }
